74eeafa6ed46b9614270f1b84833ca163759e3f1
Manuela Walczak <kolo44876@gmail.com>
2025-01-15 20:30:13 +0100

    rozw

--- main.py
+++ main.py
@@ -1,9 +1,53 @@
-#Przykład otrzymania wartości wprowadzonej przy użyciu funkcji input().
-wyraz=input()
+import math
+from collections import Counter
+import re
 
-#W celu poprawnego działania kodu w ramach GitHub Classroom warto dodatkowo użyć funkcję strip()
-#To pozwoli na usunięcie spacji oraz innych "spacjopodobnych" znaków (tabulacja \t', przejście do nowej linii '\n' lub '\r' etc.) z "głowy" i "ogona" (lewej i prawej części wyrazu).
-wyraz=wyraz.strip()
+lambda_wartosc = 0.5
 
-#Wydruk na ekranie (w konsoli)
-print ('Ten wyraz został wprowadzony:', wyraz)
+def tokenizuj(tekst):
+    return [slowo.strip() for slowo in re.split(r'[,\.\?!: ]+', tekst.lower()) if slowo]
+
+def oblicz_czestosci(dokumenty):
+    czestosci_w_dokumentach = [Counter(tokenizuj(dok)) for dok in dokumenty]
+    czestosci_korpusu = Counter(slowo for czestosci in czestosci_w_dokumentach for slowo in czestosci)
+    return czestosci_w_dokumentach, czestosci_korpusu
+
+def policz_prawdopodobienstwo(dokumenty, zapytanie, czestosci_w_dokumentach, czestosci_korpusu):
+    terminy_zapytania = tokenizuj(zapytanie)
+    dlugosc_korpusu = sum(czestosci_korpusu.values())
+    wyniki = []
+
+    for indeks, czestosci_dokumentu in enumerate(czestosci_w_dokumentach):
+        dlugosc_dokumentu = sum(czestosci_dokumentu.values())
+        log_prawdopodobienstwo = 0
+
+        for termin in terminy_zapytania:
+            p_td = czestosci_dokumentu[termin] / dlugosc_dokumentu if dlugosc_dokumentu > 0 else 0
+            p_tc = czestosci_korpusu[termin] / dlugosc_korpusu if dlugosc_korpusu > 0 else 0
+            p_t = lambda_wartosc * p_td + (1 - lambda_wartosc) * p_tc
+
+            if p_t > 0:
+                log_prawdopodobienstwo += math.log(p_t)
+            else:
+                log_prawdopodobienstwo = float('-inf')
+                break
+
+        wyniki.append((indeks, log_prawdopodobienstwo))
+
+    wyniki.sort(key=lambda x: (-x[1], x[0]))
+    return wyniki
+
+def model_prawdopodobienstwa(zapytanie, dokumenty):
+    czestosci_w_dokumentach, czestosci_korpusu = oblicz_czestosci(dokumenty)
+    wyniki = policz_prawdopodobienstwo(dokumenty, zapytanie, czestosci_w_dokumentach, czestosci_korpusu)
+    indeksy = [indeks for indeks, _ in wyniki]
+    print(indeksy)
+
+def main():
+    liczba_dokumentow = int(input())
+    dokumenty = [input() for _ in range(liczba_dokumentow)]
+    zapytanie = input()
+
+    model_prawdopodobienstwa(zapytanie, dokumenty)
+
+main()
